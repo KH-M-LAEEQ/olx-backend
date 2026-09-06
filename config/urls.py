391a585e956315f7dc37d9c5df91b9871c0d0135
Laeeq <khawajalaeeq2@gmail.com>
2026-09-06 16:08:16 +0500

    Add Google site verification route to URLs

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
+from django.http import HttpResponse
 from django.views.static import serve
 from ads.views import CategoryListView
 
@@ -8,11 +9,21 @@ admin.site.site_header = 'Bazaario Administration'
 admin.site.site_title = 'Bazaario Admin'
 admin.site.index_title = 'Bazaario Administration'
 
+GOOGLE_VERIFY = 'google68b09f5de3bed367.html'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/',       include('accounts.urls')),
     path('api/ads/',        include('ads.urls')),
     path('api/categories/', CategoryListView.as_view(), name='category-list'),
     path('api/messages/', include('messaging.urls')),
+    path(
+        GOOGLE_VERIFY,
+        lambda request: HttpResponse(
+            f'google-site-verification: {GOOGLE_VERIFY}',
+            content_type='text/html',
+        ),
+        name='google-verify',
+    ),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
